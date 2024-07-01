398cb232733e6f104e50a0ca2b6867ae076cf14f
Aniket <Aniket.Bhadauria@emerson.com>
2024-07-02 00:20:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+gsap.from(".nav",{
+    opacity:0,
+    y: -40,
+    duration: 2,
+    ease: "power2.out",
+    delay:1,
+})
+gsap.from(".logo",{
+    opacity:0,
+    y: -40,
+    duration: 2,
+    ease: "power2.out",
+    delay:1,
+})
+gsap.from("h1",{
+    opacity:0,
+    y:20,
+    duration: 2,
+    ease: "power2.out",
+    delay:2,
+})
+gsap.from("p",{
+    opacity:0,
+    y:20,
+    duration: 2,
+    ease: "power2.out",
+    delay:2.5,
+})
+gsap.from(".btn2",{
+    opacity:0,
+    y:20,
+    duration: 2,
+    ease: "power2.out",
+    delay:3.5,
+})
